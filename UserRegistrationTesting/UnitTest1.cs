a9f9ba3a4ab45d504e1fb8306eef0d3603740e49
unknown <asthaagarwal203@gmail.com>
2022-03-14 11:32:43 +0530

    [Astha] Add: UC8 Password with exactly one special character

--- UserRegistrationTesting/UnitTest1.cs
+++ UserRegistrationTesting/UnitTest1.cs
@@ -48,7 +48,7 @@ namespace UserRegistrationTesting
         public void GivenWrongPassword_ShouldReturnInvalidPassword()
         {
             string expected = "Password is not Valid";
-            string password = "Asdfdndvndfnrg";
+            string password = "As1@#$rt5";
             Pattern pattern = new Pattern();
             string actual = pattern.validatePassword(password);
             Assert.AreEqual(expected, actual);
